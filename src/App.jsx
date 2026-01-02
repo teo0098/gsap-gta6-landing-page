44d4b4df80d697603ba407acd883e235f70bda06
teo0098 <teodor.tkaczyk0407@gmail.com>
2026-01-02 14:28:40 +0100

    feat: add hero section animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
+import Hero from './sections/Hero';
+import Navbar from './sections/Navbar';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <div>
-        <h1 className="text-3xl text-indigo-600">
-          Welcome to the world of GTA and GSAP
-        </h1>
-      </div>
+      <Navbar />
+
+      <Hero />
     </main>
   );
 };
