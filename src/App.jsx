aa87efb3f942aaefd5e5db7bed8a286bc98f2acc
teo0098 <teodor.tkaczyk0407@gmail.com>
2026-01-04 12:38:16 +0100

    feat: first video animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 import Hero from './sections/Hero';
 import Navbar from './sections/Navbar';
+import FirstVideo from './sections/FirstVideo';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -11,6 +12,8 @@ const App = () => {
       <Navbar />
 
       <Hero />
+
+      <FirstVideo />
     </main>
   );
 };
