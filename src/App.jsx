52edf6a75f7f2f406226e2f92b93a8f9594fca55
teo0098 <teodor.tkaczyk0407@gmail.com>
2026-01-04 15:44:56 +0100

    feat: second video

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './sections/Hero';
 import Navbar from './sections/Navbar';
 import FirstVideo from './sections/FirstVideo';
 import Jason from './sections/Jason';
+import SecondVideo from './sections/SecondVideo';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -17,6 +18,8 @@ const App = () => {
       <FirstVideo />
 
       <Jason />
+
+      <SecondVideo />
     </main>
   );
 };
