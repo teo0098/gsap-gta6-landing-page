4554b8a9f139129ad22009160b0e6751114ed459
teo0098 <teodor.tkaczyk0407@gmail.com>
2026-01-05 17:11:37 +0100

    feat: implement Lucia section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './sections/Navbar';
 import FirstVideo from './sections/FirstVideo';
 import Jason from './sections/Jason';
 import SecondVideo from './sections/SecondVideo';
+import Lucia from './sections/Lucia';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -20,6 +21,8 @@ const App = () => {
       <Jason />
 
       <SecondVideo />
+
+      <Lucia />
     </main>
   );
 };
